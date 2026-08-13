d3d4a920c4d0c9844b5bdb355c2ce5455c359b9f
Hammed Oyedele <itz.harmid@gmail.com>
2026-08-13 04:20:14 +0100

    fix: update Server.php

--- src/Concerns/Server.php
+++ src/Concerns/Server.php
@@ -32,7 +32,7 @@ class Server
 
 		$this->serverEntry->register($actionName, $callback);
 
-		return $this->serverActionsUrl . '?action=' . $actionName;
+		return $this->serverActionsUrl . '?__action=' . $actionName;
 	}
 
 	/**
@@ -60,7 +60,7 @@ class Server
 	 */
 	public function run(): mixed
 	{
-		$actionName = $_GET['action'] ?? null;
+		$actionName = $_GET['__action'] ?? null;
 
 		if (is_null($actionName)) {
 			throw new InvalidIndexException('No action index provided.', 400);
