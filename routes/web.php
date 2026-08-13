ab52b192ed5d7f57432ef432b67ce5407713bd7c
Hammed Oyedele <itz.harmid@gmail.com>
2026-08-13 04:32:15 +0100

    fix: update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,6 @@
 <?php
 
-use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\Config;
 use DevHammed\ServerActions\Exceptions\InvalidIndexException;
 use DevHammed\ServerActions\Exceptions\InvalidServerActionException;
 use DevHammed\ServerActions\Exceptions\RequiredServerParameterException;
@@ -11,18 +9,16 @@ use function DevHammed\ServerActions\useServer;
 
 Route::group([
 	'as' => 'server-actions.',
-	'prefix' => Config::get('server-actions.route'),
-	'middleware' => Config::get('server-actions.middleware'),
+	'prefix' => config('server-actions.route'),
+	'middleware' => config('server-actions.middleware'),
 ], function() {
 	Route::post('/', function() {
 		try {
 			return useServer()->run();
 		} catch (InvalidIndexException|InvalidServerActionException $e) {
-			App::abort(404, $e->getMessage());
+			abort(404, $e->getMessage());
 		} catch (RequiredServerParameterException $e) {
-			App::abort(400, $e->getMessage());
+			abort(400, $e->getMessage());
 		}
-
-		return null;
 	})->name('handle');
 });
